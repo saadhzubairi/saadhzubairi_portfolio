aa7372c5f5520cf063ebce8c18fc822639745354
saadhzubairi <saadhzubairi@outlook.com>
2023-06-30 10:24:55 +0500

    added the qualifications section

--- src/components/qualifications/Qualifications.jsx
+++ src/components/qualifications/Qualifications.jsx
@@ -1,84 +1,140 @@
+import { useState } from "react"
 import "./qualifications.css"
 
 function Qualifications(props) {
+    const [toggle, setToggle] = useState(0);
+
+    const toggleTab = (n) => {
+        setToggle(n);
+    }
+
     return (
         <section className="qualifications section" id="qualifications">
-
             <h2 className="section__title">Qualifications</h2>
-            <span className="section__subtitle">A glimpse into my personal journey</span>
+            <span className="section__subtitle">A glimpse into my journey</span>
 
-            <div className="qualification__container container">
+            <div className="qual__container container">
 
-                <div className="qualification__tabs">
-                    <div className="qualification__button button--flex">
-                        <i className="uil uil-graduation-cap qualification__icon"></i>{"  "}Education
+                <div className="qual__tabs">
+                    <div className={toggle === 0 ? "qual__button qual__active button--flex" : "qual__button button--flex"}
+                        onClick={() => toggleTab(0)}>
+                        <i className="uil uil-graduation-cap qual__icon"></i> Education
                     </div>
-
-                    <div className="qualification__button button--flex">
-                        <i className="uil uil-briefcase qualification__icon"></i>{"  "}Experience
+                    <div className={toggle === 1 ? "qual__button qual__active button--flex" : "qual__button button--flex"}
+                        onClick={() => toggleTab(1)}>
+                        <i className="uil uil-briefcase-alt qual__icon"></i> Experience
                     </div>
                 </div>
 
-                <div className="qualification__sections">
-                    {/* ONE PART OF YOUR QUAL, COPU THIOS */}
-                    <div className="qualification__content-active">
-                        <div className="qualification__data">
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
+                <div className="qual__sections">
+                    <div className={toggle === 0 ? "qual__content qual__content-active" : "qual__content qual__content"}>
+
+                        <div className="qual__data">
                             <div>
-                                <h3 className="qualification__title">Bachelor in Computer Science</h3>
-                                <span className="qualification__subtitle">Institute of Bdawg, khi</span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i> 2021 - Present
-                                </div>
+                                <h3 className="qual__tutle">BSCS</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>19 - 2023</div>
                             </div>
-
-                        </div>
-                    </div>
-                    {/* ONE PART OF YOUR QUAL, COPU THIOS */}
-                    <div className="qualification__content-active">
-
-                        <div className="qualification__data">
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                            
                             <div>
-                                <h3 className="qualification__title">Bachelor in Computer Science</h3>
-                                <span className="qualification__subtitle">Institute of Bdawg, khi</span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i> 2021 - Present
-                                </div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
                             </div>
                         </div>
-                    </div>
-                    <div className="qualification__content-active">
-                        <div className="qualification__data">
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                            <div>
-                                <h3 className="qualification__title">Bachelor in Computer Science</h3>
-                                <span className="qualification__subtitle">Institute of Bdawg, khi</span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i> 2021 - Present
-                                </div>
-                            </div>
 
-                        </div>
-                    </div>
-                    <div className="qualification__content-active">
-                        <div className="qualification__data">
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
+                        <div className="qual__data">
+                            <div></div>
                             <div>
-                                <h3 className="qualification__title">Bachelor in Computer Science</h3>
-                                <span className="qualification__subtitle">Institute of Bdawg, khi</span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i> 2021 - Present
-                                </div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qual__tutle">BSCS</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>2019 - 2023</div>
                             </div>
                         </div>
+
+                        <div className="qual__data">
+                            <div>
+                                <h3 className="qual__tutle">BSCS</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>2019 - 2023</div>
+                            </div>
+                            <div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qual__data">
+                            <div></div>
+                            <div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qual__tutle">BSCS</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>2019 - 2023</div>
+                            </div>
+                        </div>
+
                     </div>
 
+
+                    <div className={toggle === 1 ? "qual__content qual__content-active" : "qual__content qual__content"}>
+
+                        <div className="qual__data">
+                            <div>
+                                <h3 className="qual__tutle">LALA</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>2019 - 2023</div>
+                            </div>
+                            <div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qual__data">
+                            <div></div>
+                            <div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qual__tutle">LALA</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>2019 - 2023</div>
+                            </div>
+                        </div>
+
+                        <div className="qual__data">
+                            <div>
+                                <h3 className="qual__tutle">LALA</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i class="uil uil-schedule"></i>2019 - 2023</div>
+                            </div>
+                            <div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qual__data">
+                            <div></div>
+                            <div>
+                                <span className="qual__rounder"></span>
+                                <span className="qual__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qual__tutle">LALA</h3>
+                                <span className="qual__subtitle">Institute of Business Administration, Karachi</span>
+                                <div className="qual__calendar"><i className="uil uil- qual__icon"></i>2019 - 2023</div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
