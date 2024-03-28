720fefb5c9be1ee36d039c142cbd03852d08665c
saadhzubairi <saadhzubairi@outlook.com>
2024-03-29 02:05:16 +0500

    installed added tailwind and shadcn ui

--- src/components/utils/Quillian.jsx
+++ src/components/utils/Quillian.jsx
@@ -1,50 +1,31 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import ScrollSnap from 'scroll-snap'
-import './quillian.css'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog"
 
-function callback() {
-    console.log('snapped')
+
+const Quillian = () => {
+    return (
+        <div>
+            <Dialog>
+                <DialogTrigger>Open</DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
 }
-
-export class Quillian extends React.Component {
-    container = React.createRef()
-
-    bindScrollSnap() {
-        const element = this.container.current
-        const snapElement = new ScrollSnap(element, {
-            snapDestinationY: '0%',
-        })
-
-        snapElement.bind(callback)
-    }
-
-    componentDidMount() {
-        this.bindScrollSnap()
-    }
-
-    render() {
-        return (
-            <div id="container" ref={this.container}>
-                <div className="page first-page">
-                    <div>I</div>
-                    <div className="hint">scroll down</div>
-                </div>
-                <div className="page second-page">
-                    <div>II</div>
-                </div>
-                <div className="page third-page">
-                    <div>III</div>
-                </div>
-                <div className="page fourth-page">
-                    <div>IV</div>
-                    <div className="hint">scroll up</div>
-                </div>
-            </div>
-        )
-    }
-}
-
-const rootElement = document.getElementById('root')
 
 export default Quillian
